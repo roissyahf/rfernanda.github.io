0fc9bd58079e140c1f21a0490c20a815c9c60862
roissyahf <roissyahfk@gmail.com>
2025-05-25 21:39:22 +0700

    add HRA, bold, opet projects

--- data/projects.ts
+++ data/projects.ts
@@ -106,6 +106,170 @@ To enhance user satisfaction and address the identified issues, the following re
     `,
   },
 
+  // HR Analytics
+{
+    id: "hr-analytics-dashboard",
+    title: "Solving High Attrition Rate Problem",
+    description:
+      "A dashboard built to visualize employee engagement within a fictional company called Vidtribe.",
+    image: "/projects/viz-hranalytic/cover-hranalytics.png?height=400&width=600",
+    tags: ["HR Analytics", "Attrition Rate", "People Management"],
+    categories: ["visualization"],
+    date: "July 2024",
+    fullDescription: `
+## Overview
+
+This project aims to find the cause of the high attrition rate in a fictional company called Vidtribe via dashboard building and recommend action items to solve the problem.
+
+    `,
+    background: `
+
+## Background
+Vidtribe has more than 1000 employees, spearheaded across Indonesia. Even though it has become a fairly large company, Vidtribe still has quite a lot of difficulties in managing employees. This has an impact on the high attrition rate (the ratio of the number of employees who leave to the total number of employees) up to more than 10%.
+
+To prevent this from getting worse, HR department managers need help in identifying the various factors that affect the high attrition rate. In addition, it also need to monitor the respective factor by creating a dashboard. After insights found, recommend action item so that the company can lowering the attrition rate by paying attention to the root factor found.
+    `,
+    objectives: [
+      "Extract insights from the given dataset to draw action item so that the company can reduce the high attrition rate",
+"Build a machine learning model to predict whether the employee will be stayed or resigned"
+],
+    tools: ["Scikit-learn", "Tableau"],
+    dataSource: "HR Analytics dataset",
+    dataSourceUrl: "https://github.com/dicodingacademy/dicoding_dataset/tree/main/employee",
+    methodology: `
+
+## Methodology
+- Data Understanding
+- Defining Key Business Questions (KBQs)
+- Data Preparation
+- Data Visualization
+- Dashboard Sketching
+- Analyzing Trend & Pattern
+- Suggest Recommendation
+
+`,
+    links: [
+	{ title: "Repository", url: "https://github.com/roissyahf/Employee-Turnover.git" },
+	{ title: "Tableau Dashboard", url: "https://public.tableau.com/views/HRAttritionDashboard_17205009591570/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"},
+    ],
+    conclusion: `
+## Conclusion
+Vidtribe faces a significant challenge in employee retention, particularly within the first five years of service. A consistent trend shows higher attrition rates for employees with less than five years of tenure across all departments, suggesting potential issues with onboarding, training, or initial compensation. This problem is compounded by the fact that the age group with the highest number of hires (26-35 years old) also exhibits the highest resignation rates, especially among men.
+
+Surprisingly, high-performing employees with excellent job satisfaction and work-life balance are also resigning. This challenges conventional wisdom and suggests that factors beyond typical workplace satisfaction, such as compensation, career advancement opportunities, or personal reasons, might be driving these departures. Similarly, the data indicates that neither extensive business travel nor excessive overtime are primary drivers of attrition, as the majority of resigned employees rarely traveled and a higher percentage of those who didn't work overtime resigned.
+
+## Recommendation
+Based on these insights, Vidtribe should prioritize the following actions to reduce employee turnover:
+
+1. **Strengthen Early-Career Retention**: Focus on improving the experience for new hires, particularly sales representatives and male employees aged 26-35. This involves optimizing the onboarding process, providing robust training, and ensuring competitive compensation and benefits packages during the crucial first five years.
+2. **Understand High-Performer Departures**: Implement exit interviews for all resigning employees, especially high-performers and those reporting high job satisfaction and work-life balance. This will uncover specific, underlying reasons for their departures, which may include compensation, limited growth opportunities, or personal factors.
+3. **Implement Stay Interview Programs**: Proactively gather feedback from employees who have been with the company for a while. Stay interviews can identify potential issues and areas for improvement before employees decide to leave, helping to retain more tenured staff.
+4. **Investigate Non-Overtime Resignations**: Explore why a higher percentage of employees who didn't work overtime are resigning. This could indicate issues like underutilization of skills, boredom, or a lack of engagement, even without excessive hours.
+
+`,
+  },
+
+// RFM Project
+  {
+    id: "ecommerce-rfm",
+    title: "Leveraging RFM Analysis for Customer Segmentation to Boost Customer Engagement within Olist Brazilian E-Commerce Platform",
+    description:
+      "Segment customers using RFM analysis to enhance marketing strategies and improve customer retention.",
+    image: "/projects/mixed-rfm/cover-rfmanalysis.jpg?height=400&width=600",
+    tags: ["RFM Analysis", "Customer Segmentation", "Customer Retention", "E-Commerce"],
+    categories: ["machine-learning", "visualization"],
+    date: "May 2024",
+    fullDescription: `
+    `,
+    background: `
+
+## Background
+Under the period of February 2017 to August 2018, 96.648% of Olist customers didn’t make a second purchase. Olist Brazilian E-Commerce businesses must modernize their marketing and customer strategies by personalizing them to improve their services, retain customers, and attract new ones.
+
+    `,
+    objectives: [
+      "Conduct customer segmentation according to their recency, frequency, and monetary value",
+"Better understand the characteristics of each segment, then provide a personalized marketing strategy to retain their customers"
+    ],
+    tools: ["Python", "Pandas",  "Tableau"],
+    dataSource: "90,000+ customer purchasing log from Brazilian E-Commerce",
+    dataSourceUrl: "https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce",
+    methodology: `
+
+## Methodology
+1. Define the research questions.
+2. Collect the data.
+3. Clean the data.
+4. Perform Exploratory Data Analysis (EDA).
+5. Perform Feature Preprocessing to create recency, frequency, and monetary value columns.
+6. Create RFM segments using quantiles.
+7. Define RFM segments.
+8. Group RFM segments.
+9. Analyzing RFM segments.
+10. Build a Tableau dashboard.
+`,
+    links: [
+      { title: "Repository", url: "https://github.com/roissyahf/Olist-RFM-Analysis" },
+      { title: "Slide", url: "https://docs.google.com/presentation/d/1nTQDBNtG4U9ZFvQjM8gk6ObrvQitTbaadlBpkZizszo/edit?usp=sharing"},
+      { title: "Tableau Dashboard", url: "https://public.tableau.com/views/RFM_Analysis_17065090987710/story?:language=en-US&:display_count=n&:origin=viz_share_link" },
+    ],
+    conclusion: `
+## Conclusion
+![RFM Segments](/projects/mixed-rfm/rfm-segment.png?height=400&width=600)
+
+- An analysis of 75,389 Olist customers revealed that 35.99% are "lost customers," characterized by long purchase intervals and low order volume. Additionally, 23.5% are classified as "high risk to churn" due to their recent purchase inactivity. In total, a concerning 59.49% of our customers exhibit low engagement with the platform. Given the substantial size of this segment, regaining their interest is crucial.
+- Conversely, 12.40% of customers are identified as "potential customers," signifying recent purchases but minimal spending.
+- The remaining customers are distributed across categories including "new customers," "potential loyalists," "very loyal," and "VIPs," with respective percentages of 9.19%, 8.98%, 6.80%, and 3.12%.
+- While attention should be paid to all segments, prioritizing strategies to retain "lost customers" and those at "high risk to churn" is most pressing.
+    `,
+  },
+
+  // BOLD WEB APP
+{
+    id: "bold-web-app",
+    title: "Implementation of Shallow Neural Network to Detect Political News on Web App Bold",
+    description:
+      "A Web App developed to predict whether the political news is hoax or valid",
+    image: "/projects/mixed-hoaxvalid/cover-hoaxvalid.png?height=400&width=600",
+    tags: ["Hoax Prediction", "Web App", "Media & Information"],
+    categories: ["deep-learning", "team-project", "applications"],
+    date: "October 2023",
+    fullDescription: `
+## Overview
+
+BOLD helps manage election hoax news by predicting the veracity of news articles and offering educational resources on news verification and combating hoaxes. Users can also report suspected hoaxes and provide feedback on the prediction model's accuracy.
+
+    `,
+    background: `
+
+## Background
+Between 2020 and 2021, the spread of hoaxes increased from 11.2% to 11.9%. The most common type of hoax news was about health. In 2019, which was an election year, the number of hoax news reached its highest point with 928 cases. In 2022, the number of hoax news rose again. It is expected that more and more hoax news will appear before the 2024 elections. Hoax news can cause problems like threatening the quality of democratic events, damaging voters' ability to think clearly, making the election process seem less valid, and harming community harmony, which can lead to the country falling apart. Because of these impacts, it is important to deal with hoax news.
+ 
+    `,
+    objectives: [
+  "Provide a web application that predicts the veracity of news articles to help users identify and mitigate election hoax news",
+	"Offer educational resources on news authentication methods and strategies for addressing misinformation, empowering users to critically evaluate information",
+	"Enable users to report suspected hoax news and submit feedback on model inaccuracies, facilitating continuous improvement and community-driven verification efforts"
+],
+    tools: ["Tensorflow", "Flask", "Docker", "Firebase", "ReactJS", "HTML", "CSS"],
+    dataSource: "Indonesian Fact & Hoax Political News dataset",
+    dataSourceUrl: "https://www.kaggle.com/datasets/linkgish/indonesian-fact-and-hoax-political-news",
+    methodology: `
+
+## Methodology
+Our team comprises technology enthusiasts with expertise in Data and AI, and Full Stack Development.
+
+- **The Data and AI division** is responsible for developing classification algorithms to predict the validity of news.
+- **The Back End division** is accountable for constructing endpoint APIs, establishing web application infrastructure, and utilizing Firebase hosting for web application deployment.
+- **The Front End division** is responsible for designing the web application interface using Figma, and subsequently implementing the design using HTML, CSS, and ReactJS.
+`,
+    links: [
+	{ title: "Repository", url: "https://github.com/roissyahf/hoax-news-flask-api.git" },
+  { title: "Slide", url: "https://docs.google.com/presentation/d/177iMJd2fkr2V_-keBqEoDMqqalBHLEpR/edit?usp=sharing&ouid=101324279168720809850&rtpof=true&sd=true"},
+  { title: "Web App", url: "https://tsdn23-bold.web.app/"}
+    ],
+    conclusion: ``,
+  },
 
   // Congestion Forecasting Project
   {
@@ -114,7 +278,7 @@ To enhance user satisfaction and address the identified issues, the following re
     description:
       "Built a traffic congestion forecasting model for 10 AI-powered ITCS intersections using clustering, regression, and classification algorithms.",
     image: "/projects/mixed-trafficc/cover-trafficcongestion.jpg?height=400&width=600",
-    tags: ["Clustering", "Regression", "Classification", "Streamlit"],
+    tags: ["Clustering", "Regression", "Classification", "Streamlit", "Transportation"],
     categories: ["machine-learning", "team-project", "applications"],
     date: "August 2023",
     fullDescription: `
@@ -186,61 +350,68 @@ The most important features in regression and classification models are normal s
     `,
   },
 
-// RFM Project
-  {
-    id: "ecommerce-rfm",
-    title: "Leveraging RFM Analysis for Customer Segmentation to Boost Customer Engagement within Olist Brazilian E-Commerce Platform",
+  // OPET
+{
+    id: "opet-android-app",
+    title: "OPet - Online Diagnosis for Your Pets Disease",
     description:
-      "Segment customers using RFM analysis to enhance marketing strategies and improve customer retention.",
-    image: "/projects/mixed-rfm/cover-rfmanalysis.jpg?height=400&width=600",
-    tags: ["RFM Analysis", "Customer Segmentation", "E-Commerce", "Customer Retention"],
-    categories: ["machine-learning", "visualization"],
-    date: "May 2024",
+      "An Android App developed to predict pets diseases based on symptoms",
+    image: "/projects/mixed-opet/cover-opet.jpg?height=400&width=600",
+    tags: ["Disease Prediction", "Android App", "Healthcare"],
+    categories: ["deep-learning", "team-project", "applications"],
+    date: "June 2023",
     fullDescription: `
+## Overview
+
+Developed an Android application for canine disease diagnosis, leveraging an Artificial Neural Network (ANN) with 80+ symptoms and achieving 95% prediction accuracy, accessible via a Flask API with <300ms response time, improving diagnosis speed by 30%.
     `,
     background: `
 
 ## Background
-Under the period of February 2017 to August 2018, 96.648% of Olist customers didn’t make a second purchase. Olist Brazilian E-Commerce businesses must modernize their marketing and customer strategies by personalizing them to improve their services, retain customers, and attract new ones.
+In a 2022 survey by the Rakuten Insight Center, 67% of 10,422 respondents in Indonesia
+owned pets, with cats, fish, birds, and dogs being the dominant choices. Rabies is a disease
+in pets that poses a threat to human health, causing 100-156 deaths per year in Indonesia
+(source: https://bkd.sultengprov.go.id/).
 
+The uneven distribution of veterinarians contributes to difficulties in treating cat diseases,
+resulting in 60.9% of cat deaths in Surabaya (Chazar et al., 2019). Indonesia has less than
+half of the required number of veterinarians, with only 20,000 available instead of the
+needed 70,000 (Azaliarahma, 2022).
+
+Based on the problem statement, we utilize machine learning classification algorithms and
+the native Kotlin programming language to develop an application to detect early signs of
+diseases in pets based on their symptoms. The goal of this application is to assist pet
+owners in taking care of their pets, addressing the limited availability of veterinarians in
+certain areas, and the high cost of treatment at animal clinics.
     `,
     objectives: [
-      "Conduct customer segmentation according to their recency, frequency, and monetary value",
-"Better understand the characteristics of each segment, then provide a personalized marketing strategy to retain their customers"
-    ],
-    tools: ["Python", "Pandas",  "Tableau"],
-    dataSource: "90,000+ customer purchasing log from Brazilian E-Commerce",
-    dataSourceUrl: "https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce",
+      "Develop a highly accurate pet disease detection system based on symptom analysis, providing reliable and timely insights for pet owners",
+	"Implement robust technical integration across all system components to ensure seamless functionality and optimal application performance upon deployment"
+],
+    tools: ["Tensorflow", "Google Cloud", "Kotlin"],
+    dataSource: "Canine's disease and symptoms tabular dataset",
+    dataSourceUrl: "https://github.com/OPet-OnlineDiagnosisforYourPet-sDisease/MachineLearning/tree/main/dataset",
     methodology: `
 
 ## Methodology
-1. Define the research questions.
-2. Collect the data.
-3. Clean the data.
-4. Perform Exploratory Data Analysis (EDA).
-5. Perform Feature Preprocessing to create recency, frequency, and monetary value columns.
-6. Create RFM segments using quantiles.
-7. Define RFM segments.
-8. Group RFM segments.
-9. Analyzing RFM segments.
-10. Build a Tableau dashboard.
+**1. Machine Learning:**
+Our machine learning efforts involved developing two distinct TensorFlow models. The first, an Artificial Neural Network (ANN), was trained on a one-hot encoded tabular dataset to classify dog diseases based on symptoms and deployed via Flask. The second model, a Convolutional Neural Network (CNN), addressed dog skin disease classification, leveraging data augmentation during preprocessing and deployed efficiently with FastAPI.
+
+**2. Mobile Development:**
+For mobile development, we integrated with existing APIs via Retrofit, implementing a ViewModel and LiveData architecture to effectively separate UI and business logic. This approach provides real-time feedback on data loading status, displaying buffers and error messages as needed, and seamlessly integrates Google Maps API for enhanced location-based functionalities.
+
+**3. Cloud Computing:** Our cloud computing strategy focused on deploying robust APIs on Google Cloud Run to support our machine learning models. We developed Node.js and Flask APIs for symptom-based diagnoses and a FastAPI for image-based diagnostics. User-uploaded images and profile pictures are securely stored in Cloud Storage buckets, linked directly within the application for efficient access.
+
 `,
     links: [
-      { title: "Repository", url: "https://github.com/roissyahf/Olist-RFM-Analysis" },
-      { title: "Slide", url: "https://docs.google.com/presentation/d/1nTQDBNtG4U9ZFvQjM8gk6ObrvQitTbaadlBpkZizszo/edit?usp=sharing"},
-      { title: "Tableau Dashboard", url: "https://public.tableau.com/views/RFM_Analysis_17065090987710/story?:language=en-US&:display_count=n&:origin=viz_share_link" },
+	{ title: "Repository", url: "https://github.com/OPet-OnlineDiagnosisforYourPet-sDisease" },
+  { title: "Slide", url: "https://docs.google.com/presentation/d/1mDMR53GSlRN2zSmOy2UADmQ0YRJDD_cp/edit?usp=sharing&ouid=101324279168720809850&rtpof=true&sd=true"},
+	{ title: "Demo Video", url: "https://drive.google.com/file/d/1lXz7_3klDRVMWrzm0zurpmWOxxD5O_Wg/view?usp=drive_link"},
     ],
-    conclusion: `
-## Conclusion
-![RFM Segments](/projects/mixed-rfm/rfm-segment.png?height=400&width=600)
-
-- An analysis of 75,389 Olist customers revealed that 35.99% are "lost customers," characterized by long purchase intervals and low order volume. Additionally, 23.5% are classified as "high risk to churn" due to their recent purchase inactivity. In total, a concerning 59.49% of our customers exhibit low engagement with the platform. Given the substantial size of this segment, regaining their interest is crucial.
-- Conversely, 12.40% of customers are identified as "potential customers," signifying recent purchases but minimal spending.
-- The remaining customers are distributed across categories including "new customers," "potential loyalists," "very loyal," and "VIPs," with respective percentages of 9.19%, 8.98%, 6.80%, and 3.12%.
-- While attention should be paid to all segments, prioritizing strategies to retain "lost customers" and those at "high risk to churn" is most pressing.
-    `,
+    conclusion: ``,
   },
 
+  // Atliqo Dashboard
   {
     id: "atliqo-comparison-dashboard",
     title: "Comparison Dashboard of Pre vs. Post 5G Launch in Atliqo Telecom Company",
